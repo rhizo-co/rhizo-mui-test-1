834231491fc2cc4d98be38a9971d0dae83411576
Andrew Jefferson <andrew@Andrews-iMac-Pro.local>
2021-10-22 14:16:48 +0200

    hello world with material ui, react and nextjs

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+export default function Index() {
+  return (
+<Container>
+  <Box>
+    <Button variant="contained">Hello World</Button>
+  </Box>
+</Container>
+);
+};
